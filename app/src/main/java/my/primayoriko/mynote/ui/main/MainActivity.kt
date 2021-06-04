09693aec9393eaa381b2690d851c5ce5f2bbeabe
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-06-05 06:40:07 +0700

    chore: add button listener template

--- app/src/main/java/my/primayoriko/mynote/ui/main/MainActivity.kt
+++ app/src/main/java/my/primayoriko/mynote/ui/main/MainActivity.kt
@@ -6,17 +6,32 @@ import android.view.View
 import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import my.primayoriko.mynote.R
+import my.primayoriko.mynote.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val view = binding.root
+
+        setContentView(view)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
         supportActionBar?.setHomeButtonEnabled(false)
 
-        setContentView(R.layout.activity_main)
+        binding.btnMain.setOnClickListener {
+
+        }
+
+        binding.btnAbout.setOnClickListener {
+
+        }
     }
 
 }

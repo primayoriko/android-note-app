42442b826623d1ed5a76429c5e289c387f1bbffe
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-06-05 13:04:46 +0700

    chore: add main to about navigation

--- app/src/main/java/my/primayoriko/mynote/ui/main/MainActivity.kt
+++ app/src/main/java/my/primayoriko/mynote/ui/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package my.primayoriko.mynote.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,6 +8,7 @@ import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import my.primayoriko.mynote.R
 import my.primayoriko.mynote.databinding.ActivityMainBinding
+import my.primayoriko.mynote.ui.about.AboutActivity
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -30,7 +32,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnAbout.setOnClickListener {
-
+            val intent = Intent(view.context, AboutActivity::class.java)
+//            intent.putExtra("url", news.url)
+            view.context.startActivity(intent)
         }
     }
 

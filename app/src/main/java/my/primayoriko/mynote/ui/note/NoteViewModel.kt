9c0bda475500c5fb5b9e883f708fa5916c1ebbda
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-06-06 09:05:50 +0700

    chore: create NoteViewModel fields and methods

--- app/src/main/java/my/primayoriko/mynote/ui/note/NoteViewModel.kt
+++ app/src/main/java/my/primayoriko/mynote/ui/note/NoteViewModel.kt
@@ -1,10 +1,46 @@
 package my.primayoriko.mynote.ui.note
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import my.primayoriko.mynote.domain.Note
 import my.primayoriko.mynote.repository.NoteRepository
 
 class NoteViewModel @ViewModelInject constructor(
     val noteRepository: NoteRepository
 ): ViewModel() {
+
+    var noteList: LiveData<List<Note>> = getAll(false)
+    var favouriteOnly: Boolean = false
+        get() = field
+        set(value) {
+            field = value
+            noteList = getAll(value)
+        }
+
+    fun insert(note: Note) =
+        viewModelScope.launch {
+            noteRepository.insert(note)
+        }
+
+    fun update(note: Note) =
+        viewModelScope.launch {
+            noteRepository.update(note)
+        }
+
+    fun delete(note: Note) =
+        viewModelScope.launch {
+            noteRepository.delete(note)
+        }
+
+    fun get(id: Int): LiveData<Note> = noteRepository.getById(id)
+
+    fun getAll(favouriteOnly: Boolean): LiveData<List<Note>> =
+        if (favouriteOnly) noteRepository.getAllFavouriteSortedByUpdatedTime()
+        else noteRepository.getAllSortedByUpdatedTime()
+
+    fun getAllFavourite() = noteRepository.getAllSortedByUpdatedTime()
+
 }

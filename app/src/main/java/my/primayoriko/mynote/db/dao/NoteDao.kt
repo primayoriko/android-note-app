43fd793fd2f931af6b65eef94cf309edc0a73a12
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-06-06 08:30:47 +0700

    chore: add dao and repository methods

--- app/src/main/java/my/primayoriko/mynote/db/dao/NoteDao.kt
+++ app/src/main/java/my/primayoriko/mynote/db/dao/NoteDao.kt
@@ -1,7 +1,28 @@
 package my.primayoriko.mynote.db.dao
 
-import androidx.room.Dao
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import my.primayoriko.mynote.domain.Note
 
 @Dao
 interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Update
+    suspend fun update(note: Note)
+
+    @Query("SELECT * FROM note WHERE id = :id")
+    fun getById(id: Int): LiveData<Note>
+
+    @Query("SELECT * FROM note ORDER BY updatedTime DESC")
+    fun getAllSortedByUpdatedTime(): LiveData<List<Note>>
+
+    @Query("SELECT * FROM note WHERE isFavourite = 1 ORDER BY updatedTime DESC")
+    fun getAllFavouriteSortedByUpdatedTime(): LiveData<List<Note>>
+
 }
--- app/src/main/java/my/primayoriko/mynote/repository/NoteRepository.kt
+++ app/src/main/java/my/primayoriko/mynote/repository/NoteRepository.kt
@@ -1,9 +1,26 @@
 package my.primayoriko.mynote.repository
 
+import androidx.lifecycle.LiveData
+import androidx.room.Delete
+import androidx.room.Update
 import my.primayoriko.mynote.db.dao.NoteDao
+import my.primayoriko.mynote.domain.Note
 import javax.inject.Inject
 
 class NoteRepository @Inject constructor(
     val noteDao: NoteDao
 ) {
+
+    suspend fun insert(note: Note) = noteDao.insert(note)
+
+    suspend fun delete(note: Note) = noteDao.delete(note)
+
+    suspend fun update(note: Note) = noteDao.update(note)
+
+    fun getById(id: Int): LiveData<Note> = noteDao.getById(id)
+
+    fun getAllSortedByUpdatedTime(): LiveData<List<Note>> = noteDao.getAllSortedByUpdatedTime()
+
+    fun getAllFavouriteSortedByUpdatedTime(): LiveData<List<Note>> = noteDao.getAllFavouriteSortedByUpdatedTime()
+
 }

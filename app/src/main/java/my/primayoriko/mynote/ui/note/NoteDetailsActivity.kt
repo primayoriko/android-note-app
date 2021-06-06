b8daf8ffdd41b8af5b3facd8c2e153a4b6c40b35
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-06-07 00:28:39 +0700

    fix: details info in note details page

--- app/src/main/java/my/primayoriko/mynote/ui/note/NoteDetailsActivity.kt
+++ app/src/main/java/my/primayoriko/mynote/ui/note/NoteDetailsActivity.kt
@@ -2,8 +2,12 @@ package my.primayoriko.mynote.ui.note
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import my.primayoriko.mynote.R
 import my.primayoriko.mynote.databinding.ActivityNoteDetailsBinding
 import my.primayoriko.mynote.domain.Note
+import my.primayoriko.mynote.domain.Note.NoteType
+import java.text.SimpleDateFormat
 
 class NoteDetailsActivity : AppCompatActivity() {
 
@@ -12,14 +16,37 @@ class NoteDetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val formatter = SimpleDateFormat("E, dd MMM yyyy")
 
         note = intent.getParcelableExtra("note")!!
         binding = ActivityNoteDetailsBinding.inflate(layoutInflater)
 
-        setContentView(binding.root)
-//        setSupportActionBar(binding.toolbar)
+        val view = binding.root
+
+        setContentView(view)
 
         binding.etTitle.setText(note.title)
         binding.etContent.setText(note.content)
+        binding.etCreatedTime.setText(formatter.format(note.createdTime))
+        binding.etUpdatedTime.setText(formatter.format(note.updatedTime))
+        binding.rgIsFavourite.check(
+            if(note.isFavourite) binding.rYes.id
+            else binding.rNo.id
+        )
+        binding.rgNoteType.check(
+            when(note.type) {
+                NoteType.WORK -> binding.rNoteTypeWork.id
+                NoteType.STUDY -> binding.rNoteTypeStudy.id
+                else -> binding.rNoteTypeOther.id
+            }
+        )
+
+        val imageId =
+            when(note.type) {
+                NoteType.WORK -> R.drawable.work
+                NoteType.STUDY -> R.drawable.study
+                else -> R.drawable.life
+            }
+        Glide.with(view.context).load(imageId).into(binding.ivQuote)
     }
 }

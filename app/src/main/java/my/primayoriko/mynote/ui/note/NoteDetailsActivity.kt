da151bfc5b6e9aecbbd05a700668e4d4ec5f187e
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-06-06 11:03:57 +0700

    chore: create note details layout and activity

---
+++ app/src/main/java/my/primayoriko/mynote/ui/note/NoteDetailsActivity.kt
@@ -0,0 +1,12 @@
+package my.primayoriko.mynote.ui.note
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import my.primayoriko.mynote.R
+
+class NoteDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_details)
+    }
+}

ff2f1c56b2c978eb4573871451f1a4e2f2e599b5
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-06-06 07:30:13 +0700

    fix: date entity error

--- app/src/main/java/my/primayoriko/mynote/domain/Note.kt
+++ app/src/main/java/my/primayoriko/mynote/domain/Note.kt
@@ -1,12 +1,17 @@
 package my.primayoriko.mynote.domain
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
+@Entity(tableName = "note")
 data class Note(
-    val id: Int,
     val title: String,
     val content: String,
     val createdTime: Date = Calendar.getInstance().time,
     var updatedTime: Date = Calendar.getInstance().time,
     var isFavourite: Boolean = false
-)
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+}

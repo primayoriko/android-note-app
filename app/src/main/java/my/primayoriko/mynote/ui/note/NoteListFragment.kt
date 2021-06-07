15b22b5cd015b4f056c60e1183bed517c89437c3
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-06-07 12:37:18 +0700

    feat: make responsive for main and about page

--- app/src/main/java/my/primayoriko/mynote/ui/note/NoteListFragment.kt
+++ app/src/main/java/my/primayoriko/mynote/ui/note/NoteListFragment.kt
@@ -43,23 +43,7 @@ class NoteListFragment : Fragment() {
 
         val isLandscape = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
         val gridColumnCount = if(isLandscape) 2 else 1
-//        val note1 = Note(
-//            "uhuhu",
-//            "qwerty123",
-//            NoteType.STUDY
-//        )
-//        val note2 = Note(
-//            "ahah sad sads sadas sadda",
-//            "shoot123",
-//            NoteType.WORK
-//        )
-//        val note3 = Note(
-//            "qweqweqsadsadsadsadsa",
-//            "7637 123830921 91283++",
-//            NoteType.OTHER
-//        )
-//        val myList: List<Note> = listOf(note1, note2, note3)
-//        binding.rvNote.adapter = NoteAdapter(myList)
+
         binding.rvNote.layoutManager = GridLayoutManager(context, gridColumnCount)
         viewModel.noteList.observe(viewLifecycleOwner,
             { list ->

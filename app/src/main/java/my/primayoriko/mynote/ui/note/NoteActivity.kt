edab87d1f8ed6d69d7cc6d02d1ee333948e57d4a
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-06-07 16:28:11 +0700

    feat: add seeder list

--- app/src/main/java/my/primayoriko/mynote/ui/note/NoteActivity.kt
+++ app/src/main/java/my/primayoriko/mynote/ui/note/NoteActivity.kt
@@ -1,7 +1,9 @@
 package my.primayoriko.mynote.ui.note
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -9,15 +11,21 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import my.primayoriko.mynote.R
 import my.primayoriko.mynote.databinding.ActivityNoteBinding
+import androidx.preference.PreferenceManager
+import my.primayoriko.mynote.constant.SeedData
+import timber.log.Timber
 
 @AndroidEntryPoint
 class NoteActivity : AppCompatActivity() {
+
+    private val viewModel: NoteViewModel by viewModels()
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityNoteBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        seedData()
 
         binding = ActivityNoteBinding.inflate(layoutInflater)
 
@@ -35,4 +43,19 @@ class NoteActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
+
+    private fun seedData() {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        if (prefs.getBoolean("isFirstTime", true)) {
+            viewModel.seedData(SeedData.seedList)
+            Timber.d("executed\n")
+            Timber.d("executed\n")
+            Timber.d("executed\n")
+            Timber.d("executed\n")
+            val editor = prefs.edit()
+            editor.putBoolean("isFirstTime", false)
+            editor.apply()
+        }
+    }
+
 }
